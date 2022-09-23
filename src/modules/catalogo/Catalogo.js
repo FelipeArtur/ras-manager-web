faa60d171f0e2262a18bb00e0e8df821a10f455c
Felipe Ribeiro <feliper.dev@gmail.com>
2022-09-23 10:36:32 -0300

    Catalogo

--- src/modules/catalogo/Catalogo.js
+++ src/modules/catalogo/Catalogo.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Catalogo.css"
 
+import CatalogoCard from "./components/CatalogoCard/CatalogoCard";
+
+import person from "../../assets/person.svg";
+import reunioes from "../../assets/reunioes.svg";
+
 export default class Catalogo extends React.Component {
     render() {
         return (
             <div className="ras-catalogo">
+                <CatalogoCard icon={person} title="Membros da RAS"/>
+                <CatalogoCard icon={person} title="Eventos"/>
+                <CatalogoCard icon={reunioes} title="Reuniões"/>
+                <CatalogoCard icon={person} title="Projetos"/>
             </div>
         );
     }
--- src/modules/catalogo/MembrosRAS/MembrosRAS.js
+++ src/modules/catalogo/MembrosRAS/MembrosRAS.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./MembrosRAS.css";
 
-import BaseCatalogo from "../components/BaseCatalogo"
+import BaseCatalogo from "../components/BaseCatalogo/BaseCatalogo"
 import MembroCard from "./components/MembroCard";
 
 
