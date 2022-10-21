c0073ae796957b2d9f953f1fce2aecff79603a8c
Felipe Ribeiro <feliper.dev@gmail.com>
2022-10-21 15:55:56 -0300

    Preparando ambiente para conectar ao MySQL ; configuração do ExpressJS

--- src/utils/components/CustomButton/CustomButton.js
+++ src/utils/components/CustomButton/CustomButton.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default class CustomButton extends React.Component {
     render() {
         return(
-            <button className="custom-btn">
+            <button className="custom-btn" onClick={this.props.onClick}>
                 <Link to={this.props.navTo} className="custom-btn-text">{this.props.text}</Link>
             </button>
         );
